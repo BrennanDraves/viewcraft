5c12d5fe6e01966170aab6e2fd8af2c4c66d0b1b
brennandraves <dravesbrennan@gmail.com>
2025-01-09 18:19:44 -0500

    fix Mypy so it does not throw errors on unnecessary places

--- viewcraft/apps.py
+++ viewcraft/apps.py
@@ -1,6 +1,10 @@
+"""Config for the viewcraft app."""
+
 from django.apps import AppConfig
 
 
 class ViewcraftConfig(AppConfig):
+    """Config for the viewcraft app."""
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'viewcraft'
