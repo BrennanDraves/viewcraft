6c726fbf075ffe84cb21c35331a23f5ce666b39b
brennandraves <dravesbrennan@gmail.com>
2025-01-10 01:30:09 -0500

    add initial enums

--- viewcraft/__init__.py
+++ viewcraft/__init__.py
@@ -2,10 +2,12 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from .components import Component, ComponentConfig
+    from .enums import HookType, ViewMethod
     from .types import ComponentProtocol, ConfigProtocol, ModelT, ViewT
 
 # Public API
 from .components import Component, ComponentConfig
+from .enums import HookType, ViewMethod
 from .types import ComponentProtocol, ConfigProtocol, ModelT, ViewT
 
 # Clean namespace
@@ -16,6 +18,8 @@ __all__ = [
     'ComponentConfig',
     'ComponentProtocol'
     'ConfigProtocol',
+    'HookType',
     'ModelT',
+    'ViewMethod',
     'ViewT'
 ]
