98027ba0b65ad85cdea5d95204c2bd3e26c00829
brennandraves <dravesbrennan@gmail.com>
2025-01-09 23:50:11 -0500

    revert changes for filling out after planning

--- viewcraft/__init__.py
+++ viewcraft/__init__.py
@@ -1,17 +1,11 @@
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from .types import ComponentProtocol, ViewT
+    pass
 
-from .views import ComponentMixin
-
-# API exports
-from .types import ComponentProtocol, ViewT
-from .views import ComponentMixin
-from .components import ComponentConfig, BaseComponent
-
-# Cleanup namespace
-del(TYPE_CHECKING)
+# Clean namespace
+del TYPE_CHECKING
 
 
-__all__ = ['ComponentProtocol', 'ViewT', 'ComponentMixin', 'ComponentConfig', 'BaseComponent']
+__all__ = [
+]
--- viewcraft/components/__init__.py
+++
@@ -1,4 +0,0 @@
-from .base_config import ComponentConfig
-from .base_component import BaseComponent
-
-__all__ = ['ComponentConfig', 'BaseComponent']
