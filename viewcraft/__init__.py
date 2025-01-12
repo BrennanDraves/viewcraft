29b92bb6b1bdf642941833be8aa7c2d68786e104
brennandraves <dravesbrennan@gmail.com>
2025-01-11 23:39:28 -0500

    discard package so far for rewrite attempt

--- viewcraft/__init__.py
+++ viewcraft/__init__.py
@@ -1,31 +1,4 @@
 from typing import TYPE_CHECKING
 
-if TYPE_CHECKING:
-    from .components import Component, ComponentConfig, around_hook, post_hook, pre_hook
-    from .enums import HookType, ViewMethod
-    from .types import ComponentProtocol, ConfigProtocol, ModelT, ViewT
-    from .views import ComponentMixin
-
-# Public API
-from .components import Component, ComponentConfig, around_hook, post_hook, pre_hook
-from .enums import HookType, ViewMethod
-from .types import ComponentProtocol, ConfigProtocol, ModelT, ViewT
-from .views import ComponentMixin
-
-# Clean namespace
+# Cleanup namespaces
 del TYPE_CHECKING
-
-__all__ = [
-    'around_hook',
-    'Component',
-    'ComponentConfig',
-    'ComponentMixin',
-    'ComponentProtocol'
-    'ConfigProtocol',
-    'HookType',
-    'ModelT',
-    'post_hook',
-    'pre_hook',
-    'ViewMethod',
-    'ViewT'
-]
--- viewcraft/components/__init__.py
+++
@@ -1,3 +0,0 @@
-from .base_component import Component
-from .base_config import ComponentConfig
-from .hooks import around_hook, post_hook, pre_hook
