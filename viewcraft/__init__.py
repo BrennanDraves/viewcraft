2793171f3051262b017e9b15ebfb0521a8578ee6
brennandraves <dravesbrennan@gmail.com>
2025-01-11 23:57:49 -0500

    add enums for hook types

--- viewcraft/__init__.py
+++ viewcraft/__init__.py
@@ -1,4 +1,11 @@
 from typing import TYPE_CHECKING
 
+from .enums import HookMethod, HookPhase
+
 # Cleanup namespaces
 del TYPE_CHECKING
+
+__all__ = [
+    "HookMethod",
+    "HookPhase",
+]
---
+++ viewcraft/enums.py
@@ -0,0 +1,31 @@
+from enum import Enum
+
+
+class HookMethod(str, Enum):
+    # Setup hooks
+    SETUP = "setup"
+    GET_QUERYSET = "get_queryset"
+    GET_CONTEXT_DATA = "get_context_data"
+    GET_TEMPLATE_NAMES = "get_template_names"
+
+    # HTTP method hooks
+    GET = "get"
+    POST = "post"
+
+    # Form handling hooks (for FormView/CreateView etc.)
+    GET_FORM = "get_form"
+    GET_FORM_CLASS = "get_form_class"
+    FORM_VALID = "form_valid"
+    FORM_INVALID = "form_invalid"
+
+    # Response hooks
+    RENDER_TO_RESPONSE = "render_to_response"
+
+    def __str__(self) -> str:
+        return self.value
+
+
+class HookPhase(Enum):
+    PRE = "pre"
+    AROUND = "around"
+    POST = "post"
