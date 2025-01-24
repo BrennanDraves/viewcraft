6d79b1fa0518edf824c32b62b3619b28fa258873
brennandraves <dravesbrennan@gmail.com>
2025-01-23 21:30:00 -0500

    live test filter component, and test mix with pagination

--- demo_app/views.py
+++ demo_app/views.py
@@ -2,15 +2,19 @@ from django.views.generic import ListView
 
 from demo_app.models import BlogPost
 
-from viewcraft import ComponentMixin, PaginationConfig, PaginationComponent
+from viewcraft import ComponentMixin, PaginationConfig, FilterConfig
 
 class BlogListView(ComponentMixin, ListView):
     model = BlogPost
     template_name = 'demo_app/list.html'
     components = [
-        PaginationConfig(
+                PaginationConfig(
             per_page=5,
             visible_pages=3,
             max_pages=None  # No limit
-        )
+        ),
+                FilterConfig(fields={
+            'status': ['draft', 'published', 'archived'],
+            'category': ['Technology', 'Travel', 'Food', 'Science']
+        }),
     ]
