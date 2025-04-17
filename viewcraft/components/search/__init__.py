35c6d07466a357d5c16f918235d21c312dc1efc7
brennandraves <dravesbrennan@gmail.com>
2025-04-17 15:27:04 -0400

    revert search component to attempt rewrite

--- viewcraft/components/search/__init__.py
+++
@@ -1,8 +0,0 @@
-from .component import SearchComponent
-from .config import MatchType, SearchConfig, SearchFieldSpec
-from .exceptions import (
-    SearchConfigError,
-    SearchEncodingError,
-    SearchError,
-    SearchQueryError,
-)
